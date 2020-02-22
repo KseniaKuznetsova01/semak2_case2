e5ccd0bb2a0bd44c0eaf61ac9b517911afdf0914
KseniaKuznetsova01 <ksenks.one@gmail.com>
2020-02-22 12:41:25 +0700

    part1

---
+++ case2.py
@@ -0,0 +1,9 @@
+def main():
+    while True:
+        print(os.getcwd())
+        print(MENU)
+        command = acceptCommand()
+        runCommand(command)
+        if command == QUIT
+            print('Работа программы завершена.')
+                  break
