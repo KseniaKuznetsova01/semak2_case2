6e13ab29c8f5a058e28cae417f5cd7e124fab721
KatyaPanukova <panyukowa-yekaterina@yandex.ru>
2020-02-25 20:16:19 +0300

    wwц

--- case2.py
+++ case2.py
@@ -1,26 +1,32 @@
 import local as lc
 import os, os.path
-b=0
-c=0
-list = []
+
+
 def catalog_go(path, level=1):
+
     a = 'Level=', level, 'Content', os.listdir(path)
     list.append(a)
     for i in os.listdir(path):
         if os.path.isdir(path+'\\'+i):
-            catalog_go(path+'\\'+i,level+1)
+            catalog_go(path+'\\'+i, level+1)
+
 
 path = input(lc.PATH)
+
+b = 0
+c = 0
+list = []
+
 if os.path.isfile(path):
-    b+=1
+    b += 1
 if os.path.isdir(path):
-    c+=1
+    c += 1
 menu = int(input(lc.MENU))
 catalog_go(path)
 lel = 1
 
-while menu !=7:
-    if menu ==1:
+while menu != 7:
+    if menu == 1:
         for i in list:
             if int(i[1]) == lel:
                 print(i)
