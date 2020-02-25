a7f47df20b7f034fa84d83e045cc9405f4df9df5
KseniaKuznetsova01 <ksenks.one@gmail.com>
2020-02-25 16:29:36 +0700

    part2

--- case2.py
+++ case2.py
@@ -1,15 +1,44 @@
 import local as lc
-print(lc.MENU)
-Menu = int(input())
-while Menu != 7:
-    ...
-    Menu = int(input(lc.MENU))
-def main():
-    while True:
-        print(os.getcwd())
-        print(MENU)
-        command = acceptCommand()
-        runCommand(command)
-        if command == QUIT
-            print('Работа программы завершена.')
-            break
+import os, os.path
+b=0
+c=0
+list = []
+def catalog_go(path, level=1):
+    a = 'Level=', level, 'Content', os.listdir(path)
+    list.append(a)
+    for i in os.listdir(path):
+        if os.path.isdir(path+'\\'+i):
+            catalog_go(path+'\\'+i,level+1)
+
+path = input(lc.PATH)
+if os.path.isfile(path):
+    b+=1
+if os.path.isdir(path):
+    c+=1
+menu = int(input(lc.MENU))
+catalog_go(path)
+lel = 1
+
+while menu !=7:
+    if menu ==1:
+        for i in list:
+            if int(i[1]) == lel:
+                print(i)
+        menu = int(input())
+    if menu == 2:
+        lel -= 1
+        for i in list:
+            if int(i[1]) == lel:
+                print(i)
+        menu = int(input())
+    if menu == 3:
+        lel += 1
+        for i in list:
+            if int(i[1]) == lel:
+                print(i)
+        menu = int(input())
+    if menu == 4:
+        list1 = os.listdir(dir)
+        number_files = len(list)
+        print(number_files)
+        menu = int(input())
