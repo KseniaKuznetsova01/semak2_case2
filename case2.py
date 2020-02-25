17aa6392c449e3964ffb9dfa6b7204449326fd2a
KseniaKuznetsova01 <ksenks.one@gmail.com>
2020-02-25 14:31:25 +0700

    part1

--- case2.py
+++ case2.py
@@ -1,3 +1,9 @@
+import local as lc
+print(lc.MENU)
+Menu = int(input())
+while Menu != 7:
+    ...
+    Menu = int(input(lc.MENU))
 def main():
     while True:
         print(os.getcwd())
@@ -6,4 +12,4 @@ def main():
         runCommand(command)
         if command == QUIT
             print('Работа программы завершена.')
-                  break
+            break
